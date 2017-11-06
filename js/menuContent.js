73ee1ab9467e37855fe66b5c66aced2f19878cbd
Andy Duss <mindovermiles262@gmail.com>
2017-11-05 16:34:18 -0800

    almost finished

--- js/menuContent.js
+++ js/menuContent.js
@@ -43,3 +43,67 @@ const pizzaPage =
   </th>\
   <th><p>$49</p></th>\
 </tr>'
+
+const pastaPage =
+"<tr>\
+  <th>\
+    <h4>Pasta Carbonara</h4>\
+    <p>If you don't know what this is, maybe you shouldn't order it. Serves 2.</p>\
+  </th>\
+  <th><p>$49</p></th>\
+</tr>\
+<tr>\
+  <th>\
+    <h4>Ravioli</h4>\
+    <p>Mmm Chef Boyardee. Reheated fresh in the microwave leaving lots of cold filling. Serves 0.5 (order two).</p>\
+  </th>\
+  <th><p>$49</p></th>\
+</tr>\
+<tr>\
+  <th>\
+    <h4>Spaghetti and Meatballs</h4>\
+    <p>World famous meatball spaghetti. We take the time to butcher the meat in front of you so you know it's fresh.</p>\
+  </th>\
+  <th><p>$49</p></th>\
+</tr>"
+
+const menuPage = 
+'<table class="table menu-tabs">\
+    <thead>\
+      <tr>\
+        <th><h3 id="salad-tab">Salad</h3></th>\
+        <th><h3 id="pizza-tab">Pizza</h3></th>\
+        <th><h3 id="pasta-tab">Pasta</h3></th>\
+        <th></th>\
+      </tr>\
+    </thead>\
+</table >\
+<table class="table">\
+  <tbody id="menu-content">\
+    <tr>\
+      <th>\
+        <h4>Caesar Salad</h4>\
+        <p>The original. Classic Caesar Salad: Crunchy romaine lettuce served with Caesar dressing, garden-fresh tomatoes, and homemade garlic croutons.</p>\
+      </th>\
+      <th>$29</th>\
+    </tr>\
+    <tr>\
+      <th>\
+        <h4>Wedge Salad</h4>\
+        <p>Iceburg lettuce quarter served with sliced cucumbers, carrots, red onion alongside garden-fresh tomatoes served with your choice of dressing.</p>\
+      </th>\
+      <th>$29</th>\
+    </tr>\
+    <tr>\
+      <th>\
+        <h4>Potato Salad</h4>\
+        <p>The irresistable goodness of this creamy dish will be sure to clog your arteries in no time flat. Puresome, wholesome ingredients including homemade mayonayse, russet potatoes, and locally grown chives. Served with a side of butter and your choice of soup.</p>\
+      </th>\
+      <th>$29</th>\
+    </tr>\
+  </tbody>\
+</table>'
+
+const aboutPage = '<div class="textContainer"><h5>Your hosts, Vito Bellantuono and Vincenzo Maddalena would like to welcome you to their restaurant for a unique dining experience of Authentic Italian cuisine. Buon Appetito!</h5></div>'
+const directionsPage = '<div class="textContainer"><h5>You should know how to get here by now.</h5></div>'
+
--- js/script.js
+++ js/script.js
@@ -1,20 +1,45 @@
+// Append Posts
 const content = document.getElementById("content")
 const menuContent = document.getElementById("menu-content")
+
+// Top Tabs
 const menuTab = document.getElementById("menu")
 const aboutTab = document.getElementById("about")
 const directionsTab = document.getElementById("directions")
-const saladsTab = document.getElementById("salad-tab")
-const pizzaTab = document.getElementById("pizza-tab")
 
-
-
-
-saladsTab.addEventListener("click", function() {
-  menuContent.innerHTML = ""
-  menuContent.innerHTML = saladsPage
+menuTab.addEventListener("click", function() {
+  content.innerHTML = "";
+  content.innerHTML = menuPage;
 })
 
-pizzaTab.addEventListener("click", function() {
-  menuContent.innerHTML = ""
-  menuContent.innerHTML = pizzaPage
+aboutTab.addEventListener("click", function() {
+  content.innerHTML = "";
+  content.innerHTML = aboutPage;
 })
+
+directionsTab.addEventListener("click", function() {
+  content.innerHTML = "";
+  content.innerHTML = directionsPage;
+})
+
+
+
+  // Menu Tabs
+  const saladsTab = document.getElementById("salad-tab")
+  const pizzaTab = document.getElementById("pizza-tab")
+  const pastaTab = document.getElementById("pasta-tab")
+
+  saladsTab.addEventListener("click", function() {
+    menuContent.innerHTML = "";
+    menuContent.innerHTML = saladsPage;
+  });
+
+  pizzaTab.addEventListener("click", function() {
+    menuContent.innerHTML = "";
+    menuContent.innerHTML = pizzaPage;
+  });
+
+  pastaTab.addEventListener("click", function() {
+    menuContent.innerHTML = "";
+    menuContent.innerHTML = pastaPage;
+  })
